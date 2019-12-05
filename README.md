c0b39367d0a628c37ebd15beaa13cb406f0b6d83
Tristan Pendergrass <tristanp@dropbox.com>
2019-12-05 15:27:07 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Demo at https://www.tristanpendergrass.com/friday-elm.
+
+# Development
+
+```
+$ npm install -g parcel-bundler
+$ npm start
+```
+
+# Deployment
+
+```
+$ npm run deploy
+```
---
+++ src/Main.elm
@@ -0,0 +1,55 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (Html, div, text)
+
+
+main : Program () Model Msg
+main =
+    Browser.element { init = init, update = update, view = view, subscriptions = subscriptions }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    {}
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( {}, Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div [] [ text "Why hello there" ]
